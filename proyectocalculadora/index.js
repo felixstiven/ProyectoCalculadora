006cca77f9e7da1e69fdb4f14e5473ca4ba42d29
felixstiven <felixstiven12@gmail.com>
2024-07-08 13:02:54 -0500

    actualizacion codigo en css y javascript

--- proyectocalculadora/index.js
+++ proyectocalculadora/index.js
@@ -1,32 +1,39 @@
-const botones = document.querySelectorAll("#btn");
-const pantalla = document.querySelector("#pantalla");
 
-console.log(pantalla.textContent)
+const screen = document.querySelector(".screen");
+const botones = document.querySelectorAll(".btn");
 
-botones.forEach(boton =>{
-    // console.log(boton.textContent);
-    boton.addEventListener("click",()=>{
-        const botonApretado = boton.textContent
-        console.log(pantalla.textContent)
-        if(botonApretado == "AC"){
-            pantalla.textContent = "0"
-            return
-        } else if (botonApretado == "del"){
-            if (pantalla.textContent.length ==1){
-                pantalla.textContent = "0"
-                return
-            } else{ pantalla.textContent = pantalla.textContent.slice(0,-1)
-                return
-            }
 
-        } 
-         else{
-            if(pantalla.textContent == "0"){
-                pantalla.textContent = botonApretado
-                return
-            }else{
-                pantalla.textContent = pantalla.textContent + botonApretado
-            }
+botones.forEach(boton => {
+    boton.addEventListener("click", () =>{
+        const botonApretado = boton.textContent;
+
+        if(boton.id === "c"){
+            screen.textContent = "0";
+            return;
         }
-    });
+
+        if (boton.id === "delete"){
+            if(screen.textContent.length === 1 || screen.textContent === "Error!"){
+                screen.textContent = "0";
+            }else{
+                screen.textContent = screen.textContent.slice(0, -1);
+            }
+            return;
+        }
+
+        if(boton.id === "equal"){
+            try{
+                screen.textContent = eval(screen.textContent);
+            } catch {
+                screen.textContent = "Error!";
+            }
+            return;
+        }
+
+        if (screen.textContent=== "0" || screen.textContent === "Error!" ){
+            screen.textContent= botonApretado;
+        } else{
+            screen.textContent += botonApretado;
+        }
+    })
 })
